29c3a2321eb7527562223ed3bb91bef01a644f97
luenpala <luenpala@bancolombia.com.co>
2021-08-18 00:14:51 -0500

    chore: agregando autenticacion

--- src/main/java/com/example/usuarioMs/controllers/UserController.java
+++ src/main/java/com/example/usuarioMs/controllers/UserController.java
@@ -2,12 +2,20 @@ package com.example.usuarioMs.controllers;
 
 import com.example.usuarioMs.models.User;
 import com.example.usuarioMs.repositories.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +31,37 @@ public class UserController {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(builder.path("/addProduct/{id}").buildAndExpand(user.getId()).toUri());
         return new ResponseEntity<User>(headers, HttpStatus.CREATED);
+    }
+
+    @PostMapping("user")
+    public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+
+        String token = getJWTToken(username);
+        User user = new User();
+        user.setName(username);
+        user.setToken(token);
+        return user;
+
+    }
+
+    private String getJWTToken(String username) {
+        String secretKey = "mySecretKey";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("softtekJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS256,
+                        secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
     }
 }
