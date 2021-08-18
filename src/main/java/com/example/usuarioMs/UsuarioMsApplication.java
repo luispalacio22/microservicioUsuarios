8652a62b65c30c4f96c55c346a07d09ad7df677e
Luis Palacio <luispalacio11c@gmail.com>
2021-08-17 20:39:24 -0500

    primer commmit base de datos

---
+++ src/main/java/com/example/usuarioMs/UsuarioMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.usuarioMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioMsApplication.class, args);
+	}
+
+}
